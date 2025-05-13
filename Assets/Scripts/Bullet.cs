e7806390a0d0a2742b9fff30a1e0dcc903a0fe95
Justin <justincfcristi@yahoo.com>
2025-05-13 18:06:59 +0800

    Added bullet script

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+  public BulletData data;
+
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
